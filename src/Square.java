0292ac5d54f3357db8407650caae04afe2e1be19
Bledion Zymberi <bledion.zymberi@student.uni-pr.edu>
2025-04-19 17:34:34 +0200

    Ndryshim i vogel

--- src/Square.java
+++ src/Square.java
@@ -3,29 +3,31 @@ import java.util.Set;
 
 public class Square {
     private char[][] grid = new char[5][5];
-    public Square(String key){
+
+    public Square(String key) {
         String alphabet = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
         Set<Character> used = new LinkedHashSet<>();
-        key = key.toUpperCase().replaceAll("[^A-Z]","").replace("Q","");
-        for (char c : key.toCharArray()){
+        key = key.toUpperCase().replaceAll("[^A-Z]", "").replace("Q", "");
+        for (char c : key.toCharArray()) {
             used.add(c);
         }
-        for (char c:alphabet.toCharArray()){
+        for (char c : alphabet.toCharArray()) {
             used.add(c);
         }
-        int i =0;
-        for (char c : used){
-            grid[i/5][i%5] =c;
+        int i = 0;
+        for (char c : used) {
+            grid[i / 5][i % 5] = c;
             i++;
         }
     }
-    public int[] findPosition(char c){
+
+    public int[] findPosition(char c) {
         c = Character.toUpperCase(c);
-        if (c=='Q') c='K';
-        for (int row = 0;row <5;row++){
-            for (int col = 0;col<5;col++){
-                if (grid[row][col] ==c){
-                    return new int[]{row,col};
+        if (c == 'Q') c = 'K';
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col++) {
+                if (grid[row][col] == c) {
+                    return new int[]{row, col};
                 }
             }
         }
